16963811b068542be5097b0a65d877688b83feba
Ewout den Heijer <ewout@wgdenheijer.nl>
2022-10-31 14:59:07 +0100

    first commit of notional fork

--- strava_api.py
+++ strava_api.py
@@ -9,9 +9,17 @@ token = strava_oauth2(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 # Get Strava Data
 client = StravaIO(access_token=token["access_token"])
 activities = client.get_logged_in_athlete_activities()
-
+activities = activities[::-1]
 # Upload information to Notion
 notion = NotionInterface()
-table = notion.create_activity_log_table()
+
+database = notion.create_activity_log_table_notional()
+# TODO implement more robust checking for existing entries
+# current implementation assumes no new records after newest date in Notion
+already_added = False
+
 for activity in activities:
-    notion.add_row_to_table(table, activity)
+    already_added = notion.add_row_to_database(database, activity)
+    if already_added:
+        break
+    
