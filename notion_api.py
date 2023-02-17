4c9dbc2cdf67e6027a504579fa27b6dad34e77af
Ewout den Heijer <ewout@wgdenheijer.nl>
2023-02-17 14:35:01 +0100

    updated lookup of table

--- notion_api.py
+++ notion_api.py
@@ -3,10 +3,10 @@ import notional
 # from notional import Block, Page
 from notional import schema
 from notional.iterator import EndpointIterator
-from notional.records import Database, Page
+from notional.records import Page
 from notional.orm import connected_page
-from config import TOKEN_V2, PAGE_ID, NOTION_SECRET
-from datetime import datetime, tzinfo
+from config import PAGE_ID, NOTION_SECRET
+from datetime import datetime
 
 # from table_schema import SCHEMA
 
@@ -14,30 +14,20 @@ from datetime import datetime, tzinfo
 class NotionInterface:
     def __init__(self):
         self.notional = notional.connect(auth=NOTION_SECRET)
-        self.strava_page_title = "CPC"
-        self.strava_table_title = "Strava log"
 
     def get_strava_page_by_id(self):
-        page = self.notional.pages.retrieve(PAGE_ID)
-        return page
+        return self.notional.pages.retrieve(PAGE_ID)
 
-    def get_strava_page(self):
-        strava_page = None
-        for page in self.client.get_top_level_pages():
-            if self.strava_page_title == page.title:
-                strava_page = page
+    def strava_page_has_database(self, strava_page):
+        # check if page already contains child database
+        # TODO as of current, returns first child of type child_database; resfactor into more selective?
+        for block in self.notional.blocks.children.list(strava_page):
+            if hasattr(block, "type") and block.type == "child_database":
+                return block
+            else:
+                return False
 
-        if strava_page is None:
-            raise LookupError(
-                f"Unable to find Strava page, is there a page with a "
-                f"title of '{self.strava_page_title}' in your workspace?"
-            )
-
-        return strava_page
-
-    def create_activity_log_table_notional(self):
-        # retrieve Strava page by ID
-        strava_page = self.get_strava_page_by_id()
+    def create_activity_log_table_notional(self, strava_page):
 
         # check if page already contains child database
         # TODO as of current, returns first child of type child_database; refactor into more selective?
@@ -60,14 +50,15 @@ class NotionInterface:
 
         return strava_table
 
-    def add_row_to_database(self, db: Database, data):
+    def add_row_to_database(self, id, data):
         """Adds row to Strava database, returns true if row already has been added"""
-        stravaDB = self.notional.databases.retrieve(db.id)
+        stravaDB = self.notional.databases.retrieve(id)
 
-        # retrieve activities to be iterated in order to check if new activities are already added
+        # retrieve activities to be iterated in order to check
+        # if new activities are already added
         runs = EndpointIterator(
             endpoint=self.notional.databases().query,
-            database_id=db.id,
+            database_id=id,
             sorts=[{"direction": "descending", "property": "Date"}],
         )
 
@@ -76,24 +67,23 @@ class NotionInterface:
                 run["properties"]["Date"]["date"]["start"], "%Y-%m-%d"
             ).replace(tzinfo=None)
 
-            current_run_title = run["properties"]["Title"]["title"][0]["plain_text"]
             current_run_ID = run["properties"]["ID"]["number"]
 
-            if datetime.strptime(
-                run["properties"]["Date"]["date"]["start"], "%Y-%m-%d"
-            ).replace(tzinfo=None) > data.start_date_local.replace(tzinfo=None) or (
-                current_run_ID == data.upload_id
-            ):
-                print(f"Skipped {data.name} on {data.start_date_local}; already added!")
+            if current_run_ID == data.upload_id:
+                print(
+                    f"Skipped {data.name} on {data.start_date_local}; already added!"
+                )
                 return True
         print(
             f"About to add run {data.name} on {data.start_date_local} with ID {data.upload_id}"
         )
         Run: Page = connected_page(self.notional, source_db=stravaDB)
 
-        run_record = Run.create(
+        Run.create(
             Title=data.name if hasattr(data, "name") else "",
-            Date=data.start_date_local if hasattr(data, "start_date_local") else "",
+            Date=data.start_date_local
+            if hasattr(data, "start_date_local")
+            else "",
             Type=data.type,
             Distance=data.distance,
             Duration=data.moving_time,
